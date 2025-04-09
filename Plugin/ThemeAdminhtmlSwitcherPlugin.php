03adba62230c67059312041bae6229a47c994699
Ryan Hoerr <rhoerr@users.noreply.github.com>
2025-04-09 14:21:51 -0400

    change: apply theme plugin by adminhtml DI; refactored plugin (#2)

--- Plugin/ThemeAdminhtmlSwitcherPlugin.php
+++ Plugin/ThemeAdminhtmlSwitcherPlugin.php
@@ -1,32 +1,24 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace MageOS\ThemeAdminhtmlSwitcher\Plugin;
 
-use Magento\Theme\Model\View\Design;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
-use Psr\Log\LoggerInterface;
-use Magento\Framework\App\State;
-use Magento\Framework\Exception\LocalizedException;
+use Magento\Theme\Model\View\Design;
 
 class ThemeAdminhtmlSwitcherPlugin
 {
     private $scopeConfig;
-    private $logger;
-    private $appState;
 
     /**
      * ThemeAdminhtmlSwitcherPlugin constructor.
      *
      * @param ScopeConfigInterface $scopeConfig
-     * @param LoggerInterface $logger
-     * @param State $appState
      */
-    public function __construct(ScopeConfigInterface $scopeConfig, LoggerInterface $logger, State $appState)
-    {
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
         $this->scopeConfig = $scopeConfig;
-        $this->logger = $logger;
-        $this->appState = $appState;
     }
 
     /**
@@ -38,30 +30,13 @@ class ThemeAdminhtmlSwitcherPlugin
      */
     public function beforeSetDesignTheme(Design $subject, $themeId = null)
     {
-        try {
-            if (!$this->appState->getAreaCode()) {
-                $this->logger->info(__('Setting area code to adminhtml'));
-                $this->appState->setAreaCode(\Magento\Framework\App\Area::AREA_ADMINHTML);
-            }
-        } catch (LocalizedException $e) {
-            $this->logger->info(__('Area code issue: %1', $e->getMessage()));
-            $this->appState->setAreaCode(\Magento\Framework\App\Area::AREA_ADMINHTML);
-            $this->logger->info(__('Current area code: %1', $this->appState->getAreaCode()));
-        }
+        $isEnabled = $this->scopeConfig->isSetFlag(
+            'admin/system_admin_design/enable_theme_adminhtml_m137',
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT
+        );
 
-        if ($this->appState->getAreaCode() === \Magento\Framework\App\Area::AREA_ADMINHTML) {
-            $isEnabled = $this->scopeConfig->isSetFlag(
-                'admin/system_admin_design/enable_theme_adminhtml_m137',
-                ScopeConfigInterface::SCOPE_TYPE_DEFAULT
-            );
-
-            $this->logger->info(__('Theme Adminhtml Switcher plugin triggered: %1', 
-                $isEnabled ? __('Enabled') : __('Disabled'))
-            );
-
-            if ($isEnabled) {
-                $themeId = 'MageOS/m137-admin-theme';
-            }
+        if ($isEnabled) {
+            $themeId = 'MageOS/m137-admin-theme';
         }
 
         return [$themeId];
